91b2d171fab75bcd3233344027375a81b0f5b35d
James-Ashwood <83030417+James-Ashwood@users.noreply.github.com>
2022-01-29 12:34:12 +0000

    First steps

---
+++ __main__.py
@@ -0,0 +1,37 @@
+from this import d
+from types import NoneType
+import pyautogui
+import PIL
+from time import sleep
+import random
+
+pyautogui.PAUSE = 0.01
+
+keys = {"0": "left", "1": "right", "2": "up", "3": "down"}
+
+def updateKey(key, direction):
+    ## Direction == True means up
+    if direction:
+        pyautogui.keyUp(key)
+    else:
+        pyautogui.keyDown(key)
+    
+def click():
+    pyautogui.mouseDown();
+    pyautogui.mouseUp();
+
+def rightClick():
+    pyautogui.mouseDown(button="right");
+    pyautogui.mouseUp(button="right");
+
+def checkLocations():
+    d = 0.5
+    if not (pyautogui.locateCenterOnScreen("tree.PNG", confidence=d) is None):
+        for i in [pyautogui.locateCenterOnScreen("tree.PNG", confidence=d)]:
+            pyautogui.moveTo(list(i)[0], list(i)[1])
+            click()
+
+if __name__ == "__main__":
+    print("Python Surviv IO Bot")
+    while True:
+       checkLocations()
